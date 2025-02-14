9ea3354f249d9725538f6dfe4377cf77c3a5e85c
ljmc <ljmc2304@gmail.com>
2025-02-14 14:05:52 -0600

    JuegoAhorcadoFijo desarrollado

--- JuegoAhorcadoFijo.java
+++ JuegoAhorcadoFijo.java
@@ -9,7 +9,33 @@ package lab4;
  * @author ljmc2
  */
 public class JuegoAhorcadoFijo extends JuegoAhorcadoBase{
+    
     public JuegoAhorcadoFijo(String palabraSecreta){
-        
+        this.palabraSecreta=palabraSecreta;
+        this.palabraActual="_".repeat(palabraSecreta.length());
     }
+    
+    @Override
+    public void actualizarPalabraActual(char letra){
+        StringBuilder sb=new StringBuilder(palabraActual);
+        for(int i=0;i<palabraSecreta.length();i++){
+            if(palabraSecreta.charAt(i)==letra){
+                sb.setCharAt(i,letra);
+            }
+        }
+        palabraActual=sb.toString();
+    }
+    
+    @Override
+    public boolean verificarLetra(char letra){
+        return palabraSecreta.indexOf(letra)>=0;
+    }
+
+    @Override
+    public boolean hasGanado(){
+        return palabraActual.equals(palabraSecreta);
+    }
+    
+    
 }
+

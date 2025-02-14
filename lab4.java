c0b7d85da638489f5e79cd33d3b4819bd7b4c3f5
cbarahona29 <onnhyx1g@gmail.com>
2025-02-14 14:16:12 -0600

    Interfaz GUI main

--- lab4.java
+++ lab4.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4;
 
-/**
- *
- * @author ljmc2
- */
-public class lab4 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class lab4 extends JFrame implements ActionListener {
     
+    private JButton jugarfijo, jugaralazar, salir;
+    
+    public lab4() {
+
+        setTitle("Juego");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        
+        jugarfijo = new JButton("Jugar Ahoracado Fijo");
+        jugarfijo.setBounds(170, 160, 150, 30);
+        jugarfijo.addActionListener(this);
+        add(jugarfijo);
+        
+        jugaralazar = new JButton("Jugar  Ahorcadoal azar");
+        jugaralazar.setBounds(170, 200, 150, 30);
+        jugaralazar.addActionListener(this);
+        add(jugaralazar);
+        
+        salir = new JButton("Salir");
+        salir.setBounds(170, 240, 150, 30);
+        salir.addActionListener(this);
+        add(salir);
+        
+        setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jugarfijo) {
+
+        } else if (e.getSource() == jugaralazar) {
+
+        } else if (e.getSource() == salir) {
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new lab4();
+    }
 }
